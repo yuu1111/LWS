7675d1066f34028e1060dbabacb071390e2a842f
yuu1111 <yuu210yuu@gmail.com>
2018-05-18 17:30:15 +0900

    コンソールでの検索の情報を増やした

--- LWS/Form1.cs
+++ LWS/Form1.cs
@@ -19,17 +19,28 @@ namespace LWS
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int BloodID = 50501;
-            for (int i = 0; i < 10; ++i)
+            String TextBox1 = textBox1.Text;
+            int SracheID = int.Parse(TextBox1);
+            int SeedID = 50501;
+            int ListNo = 1;
+            int PageNo;
+
+            for (int i = 0; i < SracheID; ++i)
             {
-
-                HSPRNG.Randomize(BloodID);
-                HSPRNG.ExRandomize(BloodID);
-                int var_001 = 4 + HSPRNG.Rnd(12);
-                Console.WriteLine(BloodID + "," + var_001);
-                BloodID++;
-
+            int  PageNo1 = SeedID - 50501;
+                PageNo = PageNo1 / 16 + 1;
+                HSPRNG.Randomize(SeedID);
+                HSPRNG.ExRandomize(SeedID);
+                int BloodLV = 4 + HSPRNG.Rnd(12);
+                Console.WriteLine(ListNo + "," + PageNo + "," + SeedID + "," + BloodLV);
+                SeedID++;
+                ListNo++;
             }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+       
         }
     }
 }
